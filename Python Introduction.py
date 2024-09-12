21fdf185f4b1e68ad34e8958fddc4b5a21aa69a6
ess4all <ankitk8285174895@gmail.com>
2024-09-12 09:45:56 +0530

    Create Python Introduction.py

---
+++ Python Introduction.py
@@ -0,0 +1,58 @@
+What is Python?
+Python is a general purpose high level programming language
+
+High Level - Python’s Syntax is as simple as like English.
+
+
+General Purpose : 
+1.Software Development
+2.Game Development
+3.Web Development
+4.Automation Testing
+5.Data Science
+6.Ethical Hacking
+
+
+Software Testing
+Software ->Manual Tester - Excel - Use case
+Automation Testing - SeleniumIDE
+
+
+Python’ Features:
+1.Open Source: Free of Cost ->available
+2.Multi Paradigm Programming->
+Procedural ->step wise
+Functional - >Functions support - predefined as well user defined
+OOPS->Object Oriented Programming->Class and Objects
+Class - logical entity
+Object - physical entity
+3.Dynamically Typed Programming
+4.Python has its own set
+of libraries /modules
+Python Installation
+
+
+Variables
+X = 12
+
+Naming Conventions:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
