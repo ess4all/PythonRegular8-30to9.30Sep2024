20c8108b666d63f1a4f8ac86c2da09763599f827
ess4all <ankitk8285174895@gmail.com>
2024-09-13 09:44:58 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,10 @@
+# () - function
+x = int(input("Enter No1:"))
+y = int(input("Enter No2:"))
+print("The Sum of",x,"&",y,"is",x+y)
+print("The Sum of {} and {} is {}".format(y,x,x+y))
+print(f"The Sum of {x} and {y} is {x+y}")
+#r-raw format ->
+r"/Users/sahilkumar/Downloads"
+#b-bytes form->File Handeling
+#f/F - format
