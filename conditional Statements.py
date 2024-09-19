90de48772bffde044ecbfb17fe8831a6b9d9eac7
ess4all <ankitk8285174895@gmail.com>
2024-09-19 09:51:54 +0530

    Add files via upload

---
+++ conditional Statements.py
@@ -0,0 +1,8 @@
+# if Statement
+# take a no as input and check whether a number is even or odd
+num = int(input("Enter No : "))
+# == ->equality operator
+if num%2==0:
+    print("even")
+else:
+    print("odd")
